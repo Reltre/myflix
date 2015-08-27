ba1e80a2b0794bb9a9212fb420e8c5001f907ad7
Reltre <tglippert@gmail.com>
2015-08-27 12:25:01 -0700

    test the video model saves correctly

---
+++ test/unit/models/video_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'rails_helper'
+
+RSpec.describe Video do
+  it "saves itself" do
+    futurama = Video.new(title: "Futurama",
+     description: "Lorem ipsum dolor sit amet",
+     small_cover_url: "tmp/futurama.jpg",
+     large_cover_url: "http://dummyimage.com/665x375/000000/00a2ff")
+     futurama.save
+
+    expect(Video.first.title).to eq("Futurama")
+  end
+end
