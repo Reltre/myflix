51f2b521c9e928ecb38e252b74fd7a2ae44e3e75
Reltre <tglippert@gmail.com>
2015-09-01 14:34:05 -0700

    Base implementation of search_by_title

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,10 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   validates_presence_of :title, :description
+
+  def self.search_by_title(title)
+    Video.where('title LIKE ?', "%#{title}%").order(created_at: :desc)
+  end
+
+
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,30 @@ describe Video, type: :model do
   it { should belong_to(:category) }
   it {should validate_presence_of(:title) }
   it {should validate_presence_of(:description) }
+
+  describe "#search_by_title" do
+    it "returns an empty array is there is no match" do
+      Video.create(title: "Monk", description: "detective work")
+      expect(Video.search_by_title "Doctor Who").to eq([])
+    end
+
+    it "returns an array of one Video for an exact match" do
+      monk = Video.new(title: "Monk", description: "detective work")
+      monk.save
+      expect(Video.search_by_title "Monk").to eq([monk])
+    end
+
+    it "returns an array of one Video for a partial match" do
+      family_guy = Video.new(title: "Family Guy", description: "funny stuff")
+      family_guy.save
+      expect(Video.search_by_title "Family").to eq([family_guy])
+    end
+
+    it "returns a collection of Videos of all matches ordered by craeted at" do
+      family_ties = Video.new(title: "Family Ties", description: "family time")
+      family_guy = Video.new(title: "Family Guy", description: "funny stuff")
+      family_guy.save and family_ties.save
+      expect(Video.search_by_title "Family").to eq([family_ties, family_guy])
+    end
+  end
 end
