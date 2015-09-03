adc874f34329ddbd1624974e7963be77b08daa65
Reltre <tglippert@gmail.com>
2015-09-02 20:42:48 -0700

    Add search functionality for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,4 +6,9 @@ class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
   end
+
+  def search
+    @videos = Video.search_by_title params[:q]
+    binding.pry
+  end
 end
