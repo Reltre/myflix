3b10624347d5ebd131fdd6f5032fdab26010d722
Reltre <tglippert@gmail.com>
2015-10-27 20:07:11 -0700

    integration test of user log in

--- spec/requests/user_log_in_spec.rb
+++ spec/requests/user_log_in_spec.rb
@@ -9,10 +9,18 @@ feature "User Log In", type: :request do
   end
 
   scenario "with correct credentials" do
-    visit log_in_path
+    visit(log_in_path)
     fill_in "Email Address", with: 'kalin_borges@example.com'
     fill_in "Password", with: 'password'
     click_button "Log In"
-    expect(response).to redirect_to home_path
+    expect(current_path).to eq(home_path)
+  end
+
+  scenario "with incorrect credentials" do
+    visit(log_in_path)
+    fill_in "Email Address", with: 'cookie_monster@example.com'
+    fill_in "Password", with: 'cooookkies'
+    click_button "Log In"
+    expect(current_path).to eq(log_in_path)
   end
 end
