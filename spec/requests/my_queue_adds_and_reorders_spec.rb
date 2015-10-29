ad2462a4d71ed4d7b6477494508eef6878e37e77
Reltre <tglippert@gmail.com>
2015-10-29 00:36:03 -0700

    implement feature spec for my queue functionalities

--- spec/requests/my_queue_adds_and_reorders_spec.rb
+++ spec/requests/my_queue_adds_and_reorders_spec.rb
@@ -20,12 +20,34 @@ feature "My Queue", type: :request do
     fill_in "Password", with: 'password'
     click_button "Log In"
     expect(current_path).to eq(home_path)
-
     #click the first video on home page
     click_link("Futurama")
     #Add the video to my queue
     click_link('+ My Queue')
     expect(current_path).to eq(my_queue_path)
-    
+    #Click link to futurama show page
+    click_link("Futurama")
+    #Check that add to queue button is not rendered
+    expect(page).to have_text("Futurama")
+    expect(page).not_to have_text("+ My Queue")
+    #Add South Park to My Queue
+    click_link("Videos")
+    click_link("South Park")
+    click_link('+ My Queue')
+    #Add Family Guy to My Queue
+    click_link("Videos")
+    click_link("Family Guy")
+    #change order of queue items
+    click_link('+ My Queue')
+    %w(5 4 3).each_with_index do |list_order, index|
+      fill_in "list_order_#{index + 1}" , with: list_order
+    end
+    click_button("Update Instant Queue")
+    #verify that the order has change
+    list_orders = []
+    3.times do |index|
+      list_orders << find("#list_order_#{index + 1}").value
+    end
+    expect(list_orders).to eq(["1","2","3"])
   end
 end
