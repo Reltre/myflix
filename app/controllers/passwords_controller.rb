49cf133a1fc859b69c757cc2a5ae6c81539675ed
Reltre <tglippert@gmail.com>
2016-10-30 15:03:45 -0700

    Rename password relate routes and actions. Spec for invalid token app flow.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,13 +1,13 @@
-class ForgotPasswordsController < ApplicationController
-  def forgot_password
+class PasswordsController < ApplicationController
+  def forgot
     render :forgot_password
   end
 
-  def confirm_password_reset
+  def confirm
     render :confirm_password_reset
   end
 
-  def password_reset
+  def email
     email = params[:email]
     user = User.find_by(email: email)
     if user
@@ -16,20 +16,25 @@ class ForgotPasswordsController < ApplicationController
       redirect_to confirm_password_reset_path
     else
       if email.empty?
-        flash[:danger] = "You must supply an email address." 
+        flash[:danger] = "You must supply an email address."
       else
         flash[:danger] = "There is no user registered with that email."
       end
-      render :forgot_password
+      redirect_to :forgot_password
     end
   end
 
-  def new_password
+  def show_reset
     @token = params[:token]
-    render :new_password
+    user = User.find_by(token: params[:token])
+    if user
+      render :new_password
+    else
+      redirect_to :expired_token
+    end
   end
 
-  def set_password
+  def update
     user = User.find_by(token: params[:token])
     user.update_attributes(password: params[:password], token: nil)
     redirect_to log_in_path
--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -1,51 +1,69 @@
 require 'rails_helper'
 
-describe ForgotPasswordsController do
-  describe "POST password_reset" do
+describe PasswordsController do
+  describe "POST email" do
     context "with valid email address" do
       it "should send an email with the correct token" do
         user = Fabricate(:user)
-        post :password_reset, params: { email: user.email }
+        post :email, params: { email: user.email }
         email = ActionMailer::Base.deliveries.last
         expect(email.body.raw_source).to include(user.reload.token)
       end
 
       it "should redirect to confirm password page" do
         user = Fabricate(:user)
-        post :password_reset, params: { email: user.email }
+        post :email, params: { email: user.email }
         expect(response).to redirect_to confirm_password_reset_path
       end
     end
 
-    context "with invalid email address" do
-      it "should set a flash message when email is blank" do
-        post :password_reset, params: { email: "" }
+    context "with blank email address" do
+      it "should set a flash message" do
+        post :email, params: { email: "" }
         is_expected.to set_flash[:danger].to "You must supply an email address."
       end
-      it "should set a flash message when no user is associated with supplied email." do
-        post :password_reset, params: { email: 'example@example.com' }
+
+      it "should redirect to forgot password page" do
+        post :email, params: { email: "" }
+        expect(response).to redirect_to forgot_password_path
+      end
+    end
+
+    context "with non-existing email" do
+      it "should set a flash message" do
+        post :email, params: { email: 'example@example.com' }
         is_expected.to set_flash[:danger].to "There is no user registered with that email."
+      end
+
+      it "should redirect to forgot password page" do
+        post :email, params: { email: 'example@example.com' }
+        expect(response).to redirect_to forgot_password_path
       end
     end
   end
 
-  describe "GET new_password" do
+  describe "GET show_reset" do
     it "assigns @token" do
       user = Fabricate(:user)
       user.generate_token!
       token = user.token
-      get :new_password, token: token
+      get :show_reset, token: token
       expect(assigns(:token)).to eq(token)
+    end
+
+    it "redirects to expired token page when token is invalid" do
+      get :show_reset, token: "hfkldsfHD4384vb"
+      expect(response).to redirect_to expired_token_path
     end
   end
 
-  describe "POST set_password" do
+  describe "POST update" do
     it "redirect to log in page" do
       user = Fabricate(:user)
       new_password = 'password1'
       user.generate_token!
       token = user.token
-      post :set_password, params: { password: new_password, token: token }
+      post :update, params: { password: new_password, token: token }
       expect(response).to redirect_to log_in_path
     end
 
@@ -54,7 +72,7 @@ describe ForgotPasswordsController do
       new_password = 'password1'
       user.generate_token!
       token = user.token
-      post :set_password, params: { password: new_password, token: token }
+      post :update, params: { password: new_password, token: token }
       expect(user.reload.authenticate(new_password)).to eq(user)
     end
 
@@ -63,7 +81,7 @@ describe ForgotPasswordsController do
       new_password = 'password1'
       user.generate_token!
       token = user.token
-      post :set_password, params: { password: new_password, token: token }
+      post :update, params: { password: new_password, token: token }
       expect(user.reload.token).to be_nil
     end
   end
