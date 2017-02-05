92d13fb4936a87be40f136102e4f7dc486e447c6
Reltre <tglippert@gmail.com>
2017-02-05 12:38:52 -0800

    Fix typo in production configuration file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'vast-plateau-32340.herokuapp.com/'
+    :domain         => 'vast-plateau-32340.herokuapp.com',
     :authentication => :plain
   }
   config.action_mailer.delivery_method = :smtp
