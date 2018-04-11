9848c4cf94417fd483838878ef1c544c1907c255
Reltre <tglippert@gmail.com>
2018-04-11 15:22:21 -0700

    Add test cases for charges controller.

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -2,21 +2,38 @@ require 'rails_helper'
 
 describe ChargesController do
   describe "POST create" do
+    let(:charge_instance) { instance_double(StripeWrapper::Charge) }
+    let(:charge_class) { class_double(StripeWrapper::Charge).as_stubbed_const(:transfer_nested_constants => true) }
     context "with successful charge" do
-      it "sets the success flash message" do
-        charge_instance = instance_double(StripeWrapper::Charge)
-        charge_class = class_double(StripeWrapper::Charge).as_stubbed_const(:transfer_nested_constants => true)
+      before do
         allow(charge_instance).to receive_messages(:successful? => true)
         allow(charge_class).to receive_messages(:create => charge_instance)
         post :create, params: { token: '123' }
-
+      end
+      
+      it "sets the success flash message" do
         expect(flash[:success]).to eq('Thank you for your generous support!')
       end
-      it "redirects to the new payment path"
+
+      it "redirects to the new charge path" do
+        expect(response).to redirect_to(new_charge_path)
+      end
     end
+
     context "with unsuccessful chage" do
-      it "sets the flash error message"
-      it "redirects to the new payment path"
+      before do
+        allow(charge_instance).to receive_messages(:successful? => false,  :error_message => 'Your card was declined.')
+        allow(charge_class).to receive_messages(:create => charge_instance)
+        post :create, params: { token: '123' }
+      end
+
+      it "sets the flash error message" do
+        expect(flash[:error]).to eq('Your card was declined.')
+      end
+
+      it "redirects to the new charge path" do
+        expect(response).to redirect_to(new_charge_path)
+      end
     end
   end
 end
